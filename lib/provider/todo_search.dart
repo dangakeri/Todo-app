af68e3757f691a08f4cc47d15b9bd747f6742dd3
dangakeri <dangakeri254@gmail.com>
2023-05-13 22:03:46 +0300

    todo search

---
+++ lib/provider/todo_search.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class TodoSearchState extends Equatable {
+  final String searchTerm;
+  TodoSearchState({
+    required this.searchTerm,
+  });
+  factory TodoSearchState.initial() {
+    return TodoSearchState(searchTerm: 'searchTerm');
+  }
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [searchTerm];
+
+  @override
+  bool get stringify => true;
+
+  TodoSearchState copyWith({
+    String? searchTerm,
+  }) {
+    return TodoSearchState(
+      searchTerm: searchTerm ?? this.searchTerm,
+    );
+  }
+}
+
+class TodoSearch with ChangeNotifier {
+  TodoSearchState _state = TodoSearchState.initial();
+
+  TodoSearchState get State => _state;
+  void setSearchTerm(String newSearchTerm) {
+    _state = _state.copyWith(searchTerm: newSearchTerm);
+    notifyListeners();
+  }
+}
